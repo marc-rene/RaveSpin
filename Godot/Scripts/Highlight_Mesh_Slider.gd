57cfdd7d2d95cbf4950b2114b1295d63fc1af014
marc-rene <95175463+marc-rene@users.noreply.github.com>
2025-11-25 15:28:17 +0000

    Loads more documentation, interim report

--- Godot/Scripts/Highlight_Mesh_Slider.gd
+++ Godot/Scripts/Highlight_Mesh_Slider.gd
@@ -7,46 +7,46 @@ var starting_pos : Vector3
 
 
 func _on_activation_area_entered(area: Area3D) -> void:
-	if (area.name == "Hand"):
-		print("Slider Activated")
-		active = true
-		hand_ref = area
-		starting_pos = Target_Mesh.position
-		
-	HighLight(E_ActivationStates.Pressed)
-	
+    if (area.name == "Hand"):
+        print("Slider Activated")
+        active = true
+        hand_ref = area
+        starting_pos = Target_Mesh.position
+        
+    HighLight(E_ActivationStates.Pressed)
+    
 
 func _on_activation_area_exited(area: Area3D) -> void:
-	active = false
-	#hand_ref = null
-	if (fully_exited):
-		HighLight(E_ActivationStates.Exited)
-	else:
-		HighLight(E_ActivationStates.Hoovered)
+    active = false
+    #hand_ref = null
+    if (fully_exited):
+        HighLight(E_ActivationStates.Exited)
+    else:
+        HighLight(E_ActivationStates.Hoovered)
 
 func _process(delta: float) -> void:
-	var max_pos = $"Highlight/Max point".position.z
-	var min_pos = $"Highlight/Min point".position.z
-	
-	var hand_pos = $"../../Player/XR_RightHand".position
-	if (active == false):
-		var format_string = "Rhand pos: X: %f, Y: %f, Z: %f"
-		var actual_string = format_string % [hand_pos.x, hand_pos.y, hand_pos.z]
+    var max_pos = $"Highlight/Max point".position.z
+    var min_pos = $"Highlight/Min point".position.z
+    
+    var hand_pos = $"../../Player/XR_RightHand".position
+    if (active == false):
+        var format_string = "Rhand pos: X: %f, Y: %f, Z: %f"
+        var actual_string = format_string % [hand_pos.x, hand_pos.y, hand_pos.z]
 
-		
-	if (active):
-		
-		if ($Highlight/Activation.position.z <= min_pos):
-			$Highlight/Activation.position.z = min_pos * 0.9
-		 
-		elif ($Highlight/Activation.position.z >= max_pos):
-			$Highlight/Activation.position.z = max_pos * 0.9
-		else:
-			$Highlight/Activation.position = to_local($"../../Player/XR_RightHand".position)
-			$Highlight/Activation.position.y = 0.1
-			$Highlight/Activation.position.x = 0
-			
-			Target_Mesh.global_position = $Highlight/Activation.global_position
-			var alpha = ($Highlight/Activation.position.z ) + 0.5
-			print("Moving Slider, min_pos: ", min_pos * 0.9, " max_pos: ", max_pos * 0.9, " Alpha = ", alpha)
-		#$Highlight/Activation.global_position = Target_Mesh.global_position
+        
+    if (active):
+        
+        if ($Highlight/Activation.position.z <= min_pos):
+            $Highlight/Activation.position.z = min_pos * 0.9
+         
+        elif ($Highlight/Activation.position.z >= max_pos):
+            $Highlight/Activation.position.z = max_pos * 0.9
+        else:
+            $Highlight/Activation.position = to_local($"../../Player/XR_RightHand".position)
+            $Highlight/Activation.position.y = 0.1
+            $Highlight/Activation.position.x = 0
+            
+            Target_Mesh.global_position = $Highlight/Activation.global_position
+            var alpha = ($Highlight/Activation.position.z ) + 0.5
+            print("Moving Slider, min_pos: ", min_pos * 0.9, " max_pos: ", max_pos * 0.9, " Alpha = ", alpha)
+        #$Highlight/Activation.global_position = Target_Mesh.global_position
